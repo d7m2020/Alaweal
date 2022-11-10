37067175e28a93d7e44c561a18b89c96111bacad
d7m2020 <eeaeen@gmail.com>
2022-11-10 08:27:34 +0300

    commit

--- courses.php
+++ courses.php
@@ -9,7 +9,8 @@
 //include "./include/templates/header.php";
 include"connectBymysqli.php";
 //require "./include/templates/Arabic.php";
-require "./include/templates/src/Arabic.php"
+require "./include/templates/src/Arabic.php";
+require "./include/templates/fpdf/fpdf.php";
 //include"connect.php";
 //header('Content-type: image/jpeg'); 
 ?>
@@ -26,7 +27,7 @@ $resulet=$conn->query($sql);
 //mysqli_query($conn, "SET CHARACTER SET utf8") or die('Can\'t charset in DataBase2');
 $Arabic = new \ArPHP\I18N\Arabic();
 
-$font=realpath('./Layout/webfonts/Amiri-Regular.ttf');
+$font=realpath('./Layout/webfonts/arial.ttf');
 //$text = 'بسم الله الرحمن الرحيم';
  // $text = $Arabic->utf8Glyphs($text);
  // $font  = $path.'/GD/ae_AlHor.ttf';
@@ -36,8 +37,8 @@ $font=realpath('./Layout/webfonts/Amiri-Regular.ttf');
 
     //echo "$row[traineeName]<br>";
     foreach($resulet as $row){
-    $x=200;
-    $y=200;
+    $x=800;
+    $y=650;
     $img_source=imagecreatefromjpeg($fileName);
     $textcolor=imagecolorallocate($img_source, 0, 0, 255);
     $text = $row['traineeName'];
@@ -45,8 +46,18 @@ $font=realpath('./Layout/webfonts/Amiri-Regular.ttf');
   // $text = $Arabic->utf8Glyphs($row['traineeName']);
    // imagestring($img_source,5,$x,$y,$text,$textcolor); 'Arabic Glyphs:');
    //imagettftext($img_source, 18, 0,$x, $y, $textcolor,$font, 'Arabic Glyphs:');
-    imagettftext($img_source, 18, 0,$x, $y, $textcolor,$font, $text);
+    imagettftext($img_source, 50, 0,$x, $y, $textcolor,$font, $text);
+    $x=1080;
+    $y=777;
+    $text = $row['courseName'];
+    $text=$Arabic->utf8Glyphs($text);
+    imagettftext($img_source, 50, 0,$x, $y, $textcolor,$font, $text);
     imagejpeg($img_source,"courses/$row[id].jpg");
+    $pdf=new FPDF('L','in',[11.7,8.27]);
+    $pdf->AddPage();
+    $pdf->Image("courses/$row[id].jpg",0,0,11.7,8.22);
+    $pdf->Output("courses/$row[id].pdf","F");
+    unlink("courses/$row[id].jpg");
     imagedestroy($img_source);
 }
     //echo 
